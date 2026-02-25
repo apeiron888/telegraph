ce57cf4ee9445e87136183b31c79c661d178b29b
Apeiron <153546847+apeiron888@users.noreply.github.com>
2026-02-25 20:32:57 +0300

    Add solution for A - Beautiful Matrix (trial 2)

---
+++ codeforces/a-beautiful-matrix_trial2.py
@@ -0,0 +1,7 @@
+cr, cc = 3, 3
+for i in range(1,6):
+    row = list(map(int, input().split()))
+    if 1 in row:
+        j = row.index(1) + 1
+        move = abs(i-cr)+abs(j-cc)
+        print(move)
